847bef0dde8c0551c148027f1ce4c35d05c6843b
Daniel Figueroa <danfigueroa81@gmail.com>
2022-12-08 15:06:29 -0500

    Initial Commit

---
+++ Luna-Diary/Luna_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Luna_DiaryApp.swift
+//  Luna-Diary
+//
+//  Created by Daniel Figueroa on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Luna_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Luna-DiaryUITests/Luna_DiaryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Luna_DiaryUITestsLaunchTests.swift
+//  Luna-DiaryUITests
+//
+//  Created by Daniel Figueroa on 2022-12-08.
+//
+
+import XCTest
+
+final class Luna_DiaryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
